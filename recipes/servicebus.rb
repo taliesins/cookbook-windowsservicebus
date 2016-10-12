573b97624a820eca08db14ea5dddf49aa9c227c2
Taliesin Sisson <taliesins@yahoo.com>
2016-10-12 10:11:32 +0100

    If you do an offline install, make sure that your IIS server has the correct mime types setup for serving the content e.g. MSP, MSI, EXE

--- recipes/servicebus.rb
+++ recipes/servicebus.rb
@@ -18,16 +18,6 @@ registry_key 'HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Expl
         action :create
 end
 
-webpi_product 'VC12Redist' do
-    accept_eula true
-    action :install
-end
-
-webpi_product 'WindowsFabric' do
-    accept_eula true
-    action :install
-end
-
 webpi_product 'ServiceBus_1_1,ServiceBus_1_1_CU1,ServiceBus_1_1_NETFramework46_Update' do
     accept_eula true
     action :install
